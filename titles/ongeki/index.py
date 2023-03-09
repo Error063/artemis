6761915a3fc2484da06c7d66e2c2d23d3285e062
Hay1tsme <pitok236@gmail.com>
2023-03-09 10:56:30 -0500

    add .lower() to ping requsts

--- titles/ongeki/index.py
+++ titles/ongeki/index.py
@@ -72,7 +72,7 @@ class OngekiServlet():
         return (True, f"http://{core_cfg.title.hostname}/{game_code}/$v/", f"{core_cfg.title.hostname}/")
 
     def render_POST(self, request: Request, version: int, url_path: str) -> bytes:
-        if url_path == "/ping":
+        if url_path.lower() == "/ping":
             return zlib.compress(json.dumps({'returnCode': 1}, ensure_ascii=False).encode("utf-8"))
 
         req_raw = request.content.getvalue()
