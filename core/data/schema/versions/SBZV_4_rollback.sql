a0793aa13a32d6ea16bfd97f130738a668619de5
Dniel97 <Dniel97@noreply.gitea.tendokyu.moe>
2023-04-30 23:31:13 +0200

    diva: added clear set calculation + small improvements

---
+++ core/data/schema/versions/SBZV_4_rollback.sql
@@ -0,0 +1,9 @@
+ALTER TABLE diva_profile
+    DROP cnp_cid,
+    DROP cnp_val,
+    DROP cnp_rr,
+    DROP cnp_sp,
+    DROP btn_se_eqp,
+    DROP sld_se_eqp,
+    DROP chn_sld_se_eqp,
+    DROP sldr_tch_se_eqp;
