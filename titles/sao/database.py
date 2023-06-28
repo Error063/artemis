e446816b9a26f73ae7e91db91c89a9d3c2eecc2b
Midorica <allmightyonee@gmail.com>
2023-06-28 08:24:53 -0400

    fixing issue where SaoItemData was not working

--- titles/sao/database.py
+++ titles/sao/database.py
@@ -8,5 +8,6 @@ class SaoData(Data):
     def __init__(self, cfg: CoreConfig) -> None:
         super().__init__(cfg)
 
+        self.item = SaoItemData(cfg, self.session)
         self.profile = SaoProfileData(cfg, self.session)
         self.static = SaoStaticData(cfg, self.session)
