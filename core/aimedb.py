fd6cadf2da9ce5b92dd22e6fcd913fc627f02510
Kevin Trocolli <pitok236@gmail.com>
2023-08-20 19:56:16 -0400

    adb: hotfix

--- core/aimedb.py
+++ core/aimedb.py
@@ -282,7 +282,7 @@ class AimedbProtocol(Protocol):
         
         for x in range(req.num_logs):
             self.logger.debug(f"User {req.logs[x].aime_id} logged {req.logs[x].status.name} event, credit_ct: {req.logs[x].credit_ct} bet_ct: {req.logs[x].bet_ct} won_ct: {req.logs[x].won_ct}")
-        return ADBBaseResponse(resp_code, 0x20, 1, req.head.game_id, req.head.store_id, req.head.keychip_id, req.head.protocol_ver)
+        return ADBLogExResponse.from_req(req.head)
 
     def handle_goodbye(self, data: bytes, resp_code: int) -> None:
         self.logger.info(f"goodbye from {self.transport.getPeer().host}")
