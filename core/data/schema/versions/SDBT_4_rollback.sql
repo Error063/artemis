37b1f41b44a59d8bef98ea1f8aa00a5805bc983e
Midorica <allmightyonee@gmail.com>
2023-12-15 22:21:05 -0500

    fixing the upgrade & downgrade script for chunithm

--- core/data/schema/versions/SDBT_4_rollback.sql
+++ core/data/schema/versions/SDBT_4_rollback.sql
@@ -3,7 +3,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 ALTER TABLE chuni_score_playlog
   CHANGE COLUMN isClear isClear TINYINT(1) NULL DEFAULT NULL;
 
-ALTER TABLE aime.chuni_score_best
+ALTER TABLE chuni_score_best
   CHANGE COLUMN isSuccess isSuccess TINYINT(1) NULL DEFAULT NULL ;
 
 ALTER TABLE chuni_score_playlog
--- core/data/schema/versions/SDBT_5_upgrade.sql
+++ core/data/schema/versions/SDBT_5_upgrade.sql
@@ -3,7 +3,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 ALTER TABLE chuni_score_playlog
   CHANGE COLUMN isClear isClear TINYINT(6) NULL DEFAULT NULL;
 
-ALTER TABLE aime.chuni_score_best
+ALTER TABLE chuni_score_best
   CHANGE COLUMN isSuccess isSuccess INT(11) NULL DEFAULT NULL ;
 
 ALTER TABLE chuni_score_playlog
