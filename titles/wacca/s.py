346f82a32ad101a23d4c244dc212750ef6b8c309
Hay1tsme <pitok236@gmail.com>
2024-03-12 14:20:36 -0400

    wacca: move allowed_stages into __init__ for s

--- titles/wacca/s.py
+++ titles/wacca/s.py
@@ -11,25 +11,24 @@ from titles.wacca.handlers import *
 
 
 class WaccaS(WaccaBase):
-    allowed_stages = [
-        (1513, 13),
-        (1512, 12),
-        (1511, 11),
-        (1510, 10),
-        (1509, 9),
-        (1508, 8),
-        (1507, 7),
-        (1506, 6),
-        (1505, 5),
-        (1504, 4),
-        (1503, 3),
-        (1502, 2),
-        (1501, 1),
-    ]
-
     def __init__(self, cfg: CoreConfig, game_cfg: WaccaConfig) -> None:
         super().__init__(cfg, game_cfg)
         self.version = WaccaConstants.VER_WACCA_S
+        self.allowed_stages = [
+            (1513, 13),
+            (1512, 12),
+            (1511, 11),
+            (1510, 10),
+            (1509, 9),
+            (1508, 8),
+            (1507, 7),
+            (1506, 6),
+            (1505, 5),
+            (1504, 4),
+            (1503, 3),
+            (1502, 2),
+            (1501, 1),
+        ]
 
     async def handle_advertise_GetNews_request(self, data: Dict) -> Dict:
         resp = GetNewsResponseV2()
